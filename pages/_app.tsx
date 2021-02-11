74250bd7c9b92a025937de7beca78e8324497415
Guilherme Padilha <gs.padilha@gmail.com>
2021-02-11 09:16:58 -0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import { NextRouter } from 'next/dist/next-server/lib/router/router';
+import { AppPropsType } from 'next/dist/next-server/lib/utils';
+import { PropsWithChildren } from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
+
+export default function App({
+  Component,
+  pageProps,
+}: PropsWithChildren<AppPropsType<NextRouter, {}>>) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
